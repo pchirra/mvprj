60bb5472b4d32874fb9748d79564876205b66be0
Omar <mohamed@yahoo.fr>
2013-12-09 11:51:28 +0000

    mes codes

---
+++ src/main/java/com/ensa/appl/App.java
@@ -0,0 +1,13 @@
+package com.ensa.appl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
